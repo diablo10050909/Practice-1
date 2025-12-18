ae96b21d083537a1170a2291500b09443cb47b24
diablo10050909 <yk246320@yongkye.es.kr>
2025-12-18 18:49:02 +0900

    Add basic login functionality in Practice.py
    Implement a simple login system with ID and password validation.

---
+++ Practice.py
@@ -0,0 +1,17 @@
+ID = admin
+Password = admin
+id = input("Enter the ID")
+password = input("Enter the Password")
+
+if ID and Password = id and password:
+    print("Login Success")
+    x = 0
+else:
+    print("Login Failed")
+    x = 1
+  
+if x = 1:
+    Id = input("Make your ID")
+    PassWord = input("Make your Password")
+else:
+  exit():
